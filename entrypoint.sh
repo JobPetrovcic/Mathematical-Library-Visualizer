8619b18d2f77ea7aba79793f9121a0dd9843c8b7
JobPetrovcic <job1.petrovcic@gmail.com>
2023-09-23 14:23:27 +0200

    testing prebuild haskeel docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-echo "Your language is $1."
-ls
-agda-2.6.4 --version
+#echo "Your language is $1."
+#ls /bin
+#chmod +x install-agda-sexp.sh
+
+#apt-get update
+#./install-agda-sexp.sh
+uname -a
