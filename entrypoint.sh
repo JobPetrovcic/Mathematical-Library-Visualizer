2c5f64f421d4866e91d7d263c78d28574a05b170
JobPetrovcic <job1.petrovcic@gmail.com>
2023-09-24 14:02:12 +0200

    dsd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -l
 
 echo "Your language is $1."
-echo $(ls)
+echo $(pwd)
+echo "HOME"
+echo $(ls /home)
+echo "GITHUB"
+echo $(/github)
+echo "GITHUB/HOME"
+echo $(ls /github/home)
+
 echo "24. 09 2023 2"
 
 if [ $1 = agda ];
