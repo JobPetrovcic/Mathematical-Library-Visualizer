f046f62157f6c1f91cb7372c33708eff9cb42b79
JobPetrovcic <job1.petrovcic@gmail.com>
2023-09-24 17:48:25 +0200

    ds

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@
 echo "Your language is $1."
 cd /home/VL
 echo $(pwd)
-echo "GHC"
-echo $(ls /opt/ghc/9.4.5/bin -R)
-echo "24. 09 2023 3"
+echo $PATH
+echo "24. 09 2023 4"
 
 if [ $1 = agda ];
 then
