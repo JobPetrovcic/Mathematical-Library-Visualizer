d4c061695dc435006078f8dc5a89be3c502d66fa
JobPetrovcic <job1.petrovcic@gmail.com>
2024-01-10 09:24:00 +0100

    bugfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,41 +5,41 @@ echo "8 1 2024"
 
 ls
 
-if [ $1 = agda ];
-then
-    # move lib so agda-proof-assistant can access it
-    PATH_WHERE_LIB_ASSISTANT="test_data/agda/test_lib"
-    cd ${GITHUB_WORKSPACE}/agda-proof-assistent-assistent
-    rm ${PATH_WHERE_LIB_ASSISTANT} -r
-    mv ${GITHUB_WORKSPACE}/mylib ${PATH_WHERE_LIB_ASSISTANT}
-
-    echo "CURRENT DIR"
-    ls
-    echo "END OF CURRENT DIR"
-    
-    # go through .agda-lib files in directory; take the first
-    for file in ${PATH_WHERE_LIB_ASSISTANT}/*.agda-lib;
-    do
-        # install lib
-        mkdir ~/.agda
-        touch ~/.agda/libraries
-        echo "$(pwd)/${file}" >> ~/.agda/libraries
-
-        #echo "Converting $(basename $file) to s-expressions."
-
-        # get source name
-        SOURCE_DEST=$(python3.10 ${GITHUB_WORKSPACE}/find_source.py ${file})
-
-        # create imports.agda which contains all .agda 
-        python3.10 indexer.py --directory ${PATH_WHERE_LIB_ASSISTANT}/${SOURCE_DEST} --recurse
-
-        # convert to sexp
-        /root/.local/bin/agda --sexp --sexp-dir="${GITHUB_WORKSPACE}/agda-proof-assistent-assistent/${PATH_WHERE_LIB_ASSISTANT}/sexp" -l $(basename $file .agda-lib) --include-path="${pwd}/${PATH_WHERE_LIB_ASSISTANT}" ${GITHUB_WORKSPACE}/agda-proof-assistent-assistent/${PATH_WHERE_LIB_ASSISTANT}/${SOURCE_DEST}/imports.agda
-
-        python3.10 main.py
-        
-        break
-    done
-fi
-
-ls convert_to_web
+#if [ $1 = agda ];
+#then
+#    # move lib so agda-proof-assistant can access it
+#    PATH_WHERE_LIB_ASSISTANT="test_data/agda/test_lib"
+#    cd ${GITHUB_WORKSPACE}/agda-proof-assistent-assistent
+#    rm ${PATH_WHERE_LIB_ASSISTANT} -r
+#    mv ${GITHUB_WORKSPACE}/mylib ${PATH_WHERE_LIB_ASSISTANT}
+#
+#    echo "CURRENT DIR"
+#    ls
+#    echo "END OF CURRENT DIR"
+#    
+#    # go through .agda-lib files in directory; take the first
+#    for file in ${PATH_WHERE_LIB_ASSISTANT}/*.agda-lib;
+#    do
+#        # install lib
+#        mkdir ~/.agda
+#        touch ~/.agda/libraries
+#        echo "$(pwd)/${file}" >> ~/.agda/libraries
+#
+#        #echo "Converting $(basename $file) to s-expressions."
+#
+#        # get source name
+#        SOURCE_DEST=$(python3.10 ${GITHUB_WORKSPACE}/find_source.py ${file})
+#
+#        # create imports.agda which contains all .agda 
+#        python3.10 indexer.py --directory ${PATH_WHERE_LIB_ASSISTANT}/${SOURCE_DEST} --recurse
+#
+#        # convert to sexp
+#        /root/.local/bin/agda --sexp --sexp-dir="${GITHUB_WORKSPACE}/agda-proof-assistent-assistent/${PATH_WHERE_LIB_ASSISTANT}/sexp" -l $(basename $file .agda-lib) --include-path="${pwd}/$#{PATH_WHERE_LIB_ASSISTANT}" ${GITHUB_WORKSPACE}/agda-proof-assistent-assistent/${PATH_WHERE_LIB_ASSISTANT}/${SOURCE_DEST}/imports.agda
+#
+#        python3.10 main.py
+#        
+#        break
+#    done
+#fi
+#
+#ls convert_to_web
