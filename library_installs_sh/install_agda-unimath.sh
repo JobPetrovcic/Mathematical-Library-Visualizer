35784bb48371994fd5139859c9d71b647cab052c
JobPetrovcic <job1.petrovcic@gmail.com>
2024-03-11 19:34:14 +0100

    bugfix

--- library_installs_sh/install_agda-unimath.sh
+++ library_installs_sh/install_agda-unimath.sh
@@ -8,6 +8,6 @@ cd /library_installs_files
 git clone https://github.com/UniMath/agda-unimath > /dev/null
 
 # add it to agda imports
-echo "/library_installs_files/agda-stdlib/agda-unimath.agda-lib" >> ~/.agda/libraries
+echo "/library_installs_files/agda-unimath/agda-unimath.agda-lib" >> ~/.agda/libraries
 
 echo "Installed agda-unimath."
